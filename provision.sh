b7970529ab4b7e40b33a8d569861be65f89460b3
Tom Bombadil <amanibhavam@destructuring.org>
2012-08-31 09:30:00 -0700

    like basebox

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #/ NAME
 #/     provision.sh -- vagrant shell provisioner for zendesk definition
@@ -13,9 +13,6 @@ umask 022
 # figure out the project root under which bin, lib live
 shome="/vagrant"
 cd $shome
-
-# load a jason bourne library
-source "$shome/bin/_treadstone"
 
 # entry point
 function main {
@@ -42,13 +39,6 @@ function main {
   # microwave
   bin/microwave
 }
-
-# define command line options:
-#   var name, default, description, short option
-
-# parse the command-line
-parse_command_line "$@" || exit $?
-eval set -- "${FLAGS_ARGV}"
 
 # pass arguments to entry point
 main "$@"
